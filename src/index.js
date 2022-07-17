ecbb12847974a080dd56339dd906e9d227c8fb4a
brijeshmaster <brijeshkumarmaisuriya@gmail.com>
2022-07-17 21:40:33 +0200

    improved app.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import { AppProvider } from "./Context";
 import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppProvider>
       <Router>
-        <App />
+        <App/>
       </Router>
     </AppProvider>
   </React.StrictMode>
